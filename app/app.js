6a691519dab751c5b5b9de7ac414094ae3be10a1
Stanley Kwong <jpdstan@gmail.com>
2016-11-17 13:54:28 -0800

    add new views

--- app/app.js
+++ app/app.js
@@ -3,19 +3,20 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'myApp.about'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
   $locationProvider.hashPrefix('!');
-  $routeProvider.when('/view1', {
-    templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+  $routeProvider.when('/', {
+    templateUrl: 'index.html',
+  });
+  $routeProvider.when('/about', {
+    templateUrl: 'about/about.html',
+    controller: 'AboutCtrl'
   });
   $routeProvider.when('/view2', {
     templateUrl: 'view2/view2.html',
     controller: 'View2Ctrl'
   });
-  $routeProvider.otherwise({redirectTo: '/view1'});
+  $routeProvider.otherwise({redirectTo: '/'});
 }]);
